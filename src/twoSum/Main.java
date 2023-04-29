c2bcf31181ada07b02a238311eb059ea9e40173a
vhumenny <v.humenny@gmail.com>
2023-04-29 13:08:48 +0300

    two sum with map

--- src/twoSum/Main.java
+++ src/twoSum/Main.java
@@ -1,28 +1,41 @@
 package twoSum;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         int[] nums = new int[]{2, 7, 11, 15};
-        int target = 22;
+        int target = 17;
 
         System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
 
-        int[] result = new int[2];
+//        int[] result = new int[2];
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = 1; j < nums.length; j++) {
+//                if (i != j) {
+//                    if (nums[i] + nums[j] == target) {
+//                        result[0] = i;
+//                        result[1] = j;
+//                    }
+//                }
+//            }
+//        }
+//        return result;
+
+        Map<Integer, Integer> numberAndIndex = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if (i != j) {
-                    if (nums[i] + nums[j] == target) {
-                        result[0] = i;
-                        result[1] = j;
-                    }
-                }
+            if (numberAndIndex.containsKey(target - nums[i])) {
+                return new int[]{numberAndIndex.get(target - nums[i]), i};
             }
+            numberAndIndex.put(nums[i], i);
         }
-        return result;
+        return new int[]{};
     }
+
+
 }
