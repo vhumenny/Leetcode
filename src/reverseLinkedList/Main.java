be3d51b575be92ca85f52b34cec36dad3cf7a434
vhumenny <v.humenny@gmail.com>
2023-05-13 20:04:46 +0300

    reverse linked list

---
+++ src/reverseLinkedList/Main.java
@@ -0,0 +1,28 @@
+package reverseLinkedList;
+
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode node3 = new ListNode(3);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        System.out.println(reverseList(node1));
+
+    }
+
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode previousNode = null;
+        ListNode currentNode = head;
+
+        while (currentNode != null) {
+            ListNode nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        return previousNode;
+    }
+}
