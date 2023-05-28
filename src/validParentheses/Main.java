076c0db1a95b7c45f497c6deaa8b98394c2784ef
vhumenny <v.humenny@gmail.com>
2023-05-28 19:44:12 +0300

    Valid Parentheses

--- src/validParentheses/Main.java
+++ src/validParentheses/Main.java
@@ -1,4 +1,25 @@
 package validParentheses;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
+    public static void main(String[] args) {
+        String s = "{[]}";
+
+        System.out.println(isValid2(s));
+    }
+
+    public static boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char character : s.toCharArray()) {
+            if (character == '(' || character == '{' || character == '[') stack.push(character);
+            else if (character == ')' && !stack.empty() && stack.peek() == '(') stack.pop();
+            else if (character == '}' && !stack.empty() && stack.peek() == '{') stack.pop();
+            else if (character == ']' && !stack.empty() && stack.peek() == '[') stack.pop();
+            else return false;
+        }
+        return stack.empty();
+    }
 }
